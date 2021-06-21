68e74e415d3e6bfc8ca9424f9f181ce758e37f80
Arul Pandiyan. G <arul.g@continube.com>
2021-06-21 15:52:56 +0530

    domain default values fetched from env

--- compliancecow/compliancecow/utils/constants.py
+++ compliancecow/compliancecow/utils/constants.py
@@ -3,7 +3,8 @@ import os
 SecurityContext = "X-Cow-Security-Context"
 VersionSplitter = "_"
 
-ComplinaceCowProtocol = "https"
-ComplinaceCowHostName = "dev.compliancecow.live"
+ComplinaceCowProtocol = os.getenv("COW_SERVER_PROTOCOL", "https")
+ComplinaceCowHostName = os.getenv("COW_SERVER_HOST", "dev.compliancecow.live")
+
 
 Items = "items"
