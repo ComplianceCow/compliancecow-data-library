99c340ae2e2903ecf9a06f19e5b3e044de5cbd21
Arul Pandiyan. G <arul.g@continube.com>
2021-12-21 09:04:25 +0530

    oauth2 support added

--- compliancecow/compliancecow/utils/authutils.py
+++ compliancecow/compliancecow/utils/authutils.py
@@ -1,23 +1,48 @@
-import math
+import requests
 
-from compliancecow.utils import dictutils
+from compliancecow.utils import dictutils, wsutils
 
 
 def authorize_client(client):
-    # authorisation logic should be implemented
-    return client
+    client.auth_token = ""
+    req_data = {
+        "grant_type": "client_credentials"
+    }
+    if client.client_id and client.client_secret:
+        req_data['client_id'] = client.client_id
+        req_data['client_secret'] = client.client_secret
+    elif client.credentials.client_id and client.credentials.client_secret:
+        req_data['client_id'] = client.credentials.client_id
+        req_data['client_secret'] = client.credentials.client_secret
+
+    url_path = wsutils.get_api_url(
+        client.credentials.protocol, client.credentials.domain)
+    if url_path:
+        url_path += "v1/oauth2/token"
+        response = requests.post(url_path, data=req_data)
+        response_json = response.json()
+        if dictutils.is_valid_key(response_json, "tokenType") and dictutils.is_valid_key(response_json, "authToken"):
+            client.auth_token = response_json["tokenType"] + \
+                " "+response_json["authToken"]
+            return
+
+    raise Exception("Not a valid credential")
 
 
 def with_retry_for_auth_failure(fn):
     def retry(*kwargs):
-        # print('inside utils')
         client = kwargs[0]
         newkwargs = kwargs[1:]
         response = fn(*newkwargs)
         if dictutils.is_valid_key(response, "Message") and response['Message'] == 'UNAUTHORIZED':
-            return {'error': 'Token expired'}
-        # if dictutils.is_valid_key(response, "Message") or response.status_code:
-        #     authorize_client(client)
-        #     response = fn(*newkwargs)
+            if (client.client_id and client.client_secret) or (client.credentials.client_id and client.credentials.client_secret):
+                if len(newkwargs) >= 2 and isinstance(newkwargs[2], str):
+                    newkwargslist = list(newkwargs)
+                    newkwargslist[2] = client.auth_token
+                    newkwargs = tuple(newkwargslist)
+                authorize_client(client)
+                response = fn(*newkwargs)
+            else:
+                return {'error': 'Token expired'}
         return response
     return retry
